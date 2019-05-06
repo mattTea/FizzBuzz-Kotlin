d71cf3a28fe11a660db9cb1ba40cb7590986a661
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-05-06 22:09:14 +0100

    Adds TDD'd FizzBuzz
    - I'm yet to add the actual function into a separate file

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.31"
+}
+
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
