7d22de8280922c45234be670a0b50c00144e44aa
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-05-16 18:59:56 +0100

    Updates fizzBuzz to use when

--- src/main/kotlin/FizzBuzz.kt
+++ src/main/kotlin/FizzBuzz.kt
@@ -1,16 +1,27 @@
 class FizzBuzz {
 
+//    fun fizzBuzz(i: Int): Any {
+//        if (i % 15 === 0) {
+//            return "FizzBuzz"
+//        } else if (i % 3 === 0) {
+//            return "Fizz"
+//        } else if (i % 5 === 0) {
+//            return "Buzz"
+//        } else {
+//            return i
+//        }
+//    }
+
     fun fizzBuzz(i: Int): Any {
-        if (i % 15 === 0) {
-            return "FizzBuzz"
-        } else if (i % 3 === 0) {
-            return "Fizz"
-        } else if (i % 5 === 0) {
-            return "Buzz"
-        } else {
-            return i
+        when (i % 15 == 0) {
+            true -> return "FizzBuzz"
+        }
+        when (i % 3 == 0) {
+            true -> return "Fizz"
+        }
+        when (i % 5 == 0) {
+            true -> return "Buzz"
+            false -> return i
         }
     }
 }
-
-
